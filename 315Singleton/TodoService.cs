acdbdc370c91865d0b62f27eef28f5916fa41e40
RobertJMitchell <rjrocks32@gmail.com>
2015-03-24 11:14:43 -0500

    added new CreateToDo command in TodoService

--- 315Singleton/TodoService.cs
+++ 315Singleton/TodoService.cs
@@ -22,5 +22,44 @@ namespace _315Singleton
             }
         }
         private List<Todo> _todos { get; set; }
+
+        private int _seedId;
+
+        private TodoService()
+        {
+            _seedId = 1;
+            _todos = new List<Todo>();
+        }
+
+        //read and get all Todos in list
+        public List<Todo> GetTodos()
+        {
+            return _todos;
+        }
+
+        // read get for a single Todo item
+
+        public Todo GetTodoItem(int id)
+        {
+            return _todos.FirstOrDefault(t => t.Id == id);
+        
+        }
+
+        //Create/POST - Create a new Todo
+
+        public int CreateToDo(string task)
+        {
+            Todo nt = new Todo()
+            {
+                Id = _seedId,
+                Task = task,
+                isCompleted = false
+
+            };
+            _todos.Add(nt);
+            _seedId++;
+            return nt.Id;
+        }
+
     }
 }
