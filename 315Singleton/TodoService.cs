3121ec53ebfed265db328515d7c0edbf29bcfeb6
RobertJMitchell <rjrocks32@gmail.com>
2015-03-24 10:51:18 -0500

    updated listviews and new instance

--- 315Singleton/TodoService.cs
+++ 315Singleton/TodoService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _315Singleton.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,13 +10,17 @@ namespace _315Singleton
     {
         private static TodoService _instance = null;
 
-        public static TodoService Instance { get { 
-            if(_instance == null){
-                _instance = new TodoService();
+        public static TodoService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new TodoService();
+                }
+                return _instance;
             }
-
-            } 
         }
-
+        private List<Todo> _todos { get; set; }
     }
 }
